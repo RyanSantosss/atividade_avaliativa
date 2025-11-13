e1db9605d4054f9a445cfd2dc7afa69ccc802446
RyanSantosss <ryansantoswkl@gmail.com>
2025-11-13 08:36:28 -0300

    Criado projeto com scroll view

---
+++ app/src/main/java/com/example/cardapioproject/TelaAlimentosBackEnd.kt
@@ -0,0 +1,11 @@
+package com.example.cardapioproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TelaAlimentosBackEnd : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.tela_alimentos_front_end)
+    }
+}
